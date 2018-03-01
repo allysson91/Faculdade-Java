40a309b0c215b5dcb094d21732c4f4855e07f5d5
allysson <allysson@allysson-Aspire-A515-51G>
2018-03-01 20:10:16 -0300

    exercicioherança

---
+++ Faculdade/src/Herenca/Pessoa.java
@@ -0,0 +1,29 @@
+package Herenca;
+
+public class Pessoa {
+
+	private String nome, endereco;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		if (endereco.length() <= 80) {
+		this.endereco = endereco;
+	}else {
+		
+		throw new IllegalArgumentException("Endereço supera o limite");
+		
+	}
+	}
+	
+}
