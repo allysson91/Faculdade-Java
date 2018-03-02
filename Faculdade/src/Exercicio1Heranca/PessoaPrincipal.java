f7c39f20074b5b88c97a45f8612b3b8b1341fe6f
allysson <allysson@allysson-Aspire-A515-51G>
2018-03-02 03:02:05 -0300

    fim da aula

--- Faculdade/src/Exercicio1Heranca/PessoaPrincipal.java
+++ Faculdade/src/Exercicio1Heranca/PessoaPrincipal.java
@@ -1,9 +1,16 @@
 package Exercicio1Heranca;
 
+import java.util.Scanner;
+
 public class PessoaPrincipal {
 
 	public static void main(String[] args) {
 
+		new PessoaPrincipal().melhorarMetodo();
+
+	}
+
+	public void piorMetodo() {
 		try {
 
 			PessoaFisica pessoaFi = new PessoaFisica();
@@ -27,6 +34,46 @@ public class PessoaPrincipal {
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+
+	}
+
+	public void melhorarMetodo() {
+
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+
+			System.out.println("1 - Pessoa Física");
+			System.out.println("2 - Pessoa Juridica");
+			System.out.println("9 - Sair");
+			System.out.println("Escolha uma opção: ");
+			int op = scan.nextInt();
+			switch (op) {
+			case 1:
+
+				PessoaFisica pessoaFi = new PessoaFisica();
+				System.out.println("Informe seu cpf: ");
+				pessoaFi.setCpf(scan.next());
+				System.out.println("Informe o endereço: ");
+				pessoaFi.setEndereco(scan.next());
+				System.out.println("Informe o nome: ");
+				pessoaFi.setNome(scan.next());
+				System.out.println("Informe o rg: ");
+				pessoaFi.setRg(scan.next());
+				pessoaFi.imprimir();
+				break;
+			case 2:
+
+				break;
+
+			case 9:
+				System.exit(0);
+
+			default:
+				System.out.println("Opção invalida");
+				break;
+			}
+		}
+
 	}
 
 }
